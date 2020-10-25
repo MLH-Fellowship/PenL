a05a84bc0a78f04c4e7648414053cb5e0f6e1750
Nyior Clement Jr <cnyior27@gmail.com>
2020-10-25 19:39:25 +0100

    added sockets

--- PenL-frontend/src/main.js
+++ PenL-frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router/index";
+import socketio from 'socket.io';
+import VueSocketIO from 'vue-socket.io';
 
 Vue.config.productionTip = false;
+
+export const SocketInstance = socketio('http://localhost:8000');
+
+Vue.use(VueSocketIO, SocketInstance)
 
 new Vue({
   router,
