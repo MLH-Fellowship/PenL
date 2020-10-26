89e0b9f268b3842825a35ebba77d60f42313ec94
Nyior Clement Jr <cnyior27@gmail.com>
2020-10-26 03:10:51 +0100

    finally sockets now works...hahaha

--- PenL-frontend/src/App.vue
+++ PenL-frontend/src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <NavbarComponent v-if="show_nav" />
     <router-view />
+
+    <div v-if="connected" class="text-center">
+        <h1>I got this response: {{ message }}-- from the server</h1>
+    </div>
     <Footer v-if="show_footer" />
   </div>
 </template>
@@ -10,6 +14,7 @@
 import NavbarComponent from "@/components/Navigation/Navbar.vue";
 import Footer from "@/components/Navigation/Footer.vue";
 
+
 export default {
   name: "App",
   components: {
@@ -17,10 +22,19 @@ export default {
     Footer
   },
 
+  sockets: {
+    connect: function () {
+        console.log('socket connected to server')
+    }
+  },
+
   data() {
     return {
       show_footer: true,
-      show_nav: true
+      connected: false,
+      show_nav: true,
+      message: null,
+      
     };
   },
 
@@ -38,6 +52,15 @@ export default {
         this.show_nav = true;
       }
     }
+  },
+
+  mounted() {
+    this.sockets.subscribe("success", data => {
+        this.connected = true;
+        this.message = data.message;
+    });
+
+    this.$socket.emit("users");
   }
 };
 </script>
--- PenL-frontend/src/main.js
+++ PenL-frontend/src/main.js
@@ -1,14 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router/index";
-import socketio from 'socket.io';
+// import io from "socket.io-client";
+// socket = io.connect("http://localhost:8000");
+
+import SocketIO from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
 
 Vue.config.productionTip = false;
 
-export const SocketInstance = socketio('http://localhost:8000');
-
-Vue.use(VueSocketIO, SocketInstance)
+Vue.use(
+    new VueSocketIO({
+    debug: true,
+    connection: SocketIO('http://localhost:8000')
+    
+  })
+);
 
 new Vue({
   router,
