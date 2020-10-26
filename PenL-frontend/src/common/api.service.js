becdca560c32f7f4a8480d26a6908213770e0ec9
Nyior Clement Jr <cnyior27@gmail.com>
2020-10-26 17:43:49 +0100

    changed url to heroku backend

--- PenL-frontend/src/common/api.service.js
+++ PenL-frontend/src/common/api.service.js
@@ -1,9 +1,9 @@
 import { CSRF_TOKEN } from "./csrf_token.js";
 import axios from "axios";
 
-// https://penl-backend.herokuapp.com/
+// http://localhost:8000
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://penl-backend.herokuapp.com";
 
 function apiService(endpoint, method, data) {
   endpoint = `${API_URL}/${endpoint}`;
