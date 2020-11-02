7d6e43e124b8f53bf3bfd6bfd680c218a710b9cc
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-02 07:08:20 +0100

    fix error

--- PenL-frontend/src/main.js
+++ PenL-frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router/index";
-import { API_URL } from "@/common/api.service.js";
 
 import SocketIO from "socket.io-client";
 import VueSocketIO from "vue-socket.io";
@@ -11,7 +10,7 @@ Vue.config.productionTip = false;
 Vue.use(
   new VueSocketIO({
     debug: true,
-    connection: SocketIO(API_URL)
+    connection: SocketIO("https://penl-backend.herokuapp.com/")
   })
 );
 
